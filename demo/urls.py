37d60db9931f439f65b0915135c17e2a386649fe
sameeprungtaSB <sameep.rungta@sbinfowaves.in>
2024-08-30 16:34:50 +0530

    Revert "Initial Commit"
    This reverts commit 0119bbf1c47a31bbac69a6f36bc98a8b2cc99e3c.
    
    Commit to initial

--- demo/urls.py
+++
@@ -1,8 +0,0 @@
-# demo/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('login.urls')),  # Include login app URLs
-]
--- login/urls.py
+++
@@ -1,9 +0,0 @@
-# login/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('home/', views.home_view, name='home'),
-    path('register/', views.register_view, name='register'),
-]
